738792b91481776d013b432811a8a55f3f9788a7
justintrotta <justindavidtrotta@gmail.com>
2022-01-22 02:00:02 -0500

    added exception handling for form inputs (do they work though?) and changed wordwrap on rtb_output for readability

--- OAI-BOT/Form1.Designer.cs
+++ OAI-BOT/Form1.Designer.cs
@@ -30,6 +30,14 @@
         {
             this.rtb_output = new System.Windows.Forms.RichTextBox();
             this.btn_generate = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // rtb_output
@@ -40,7 +48,6 @@
             this.rtb_output.Size = new System.Drawing.Size(1029, 575);
             this.rtb_output.TabIndex = 0;
             this.rtb_output.Text = "";
-            this.rtb_output.WordWrap = false;
             // 
             // btn_generate
             // 
@@ -55,21 +62,106 @@
             this.btn_generate.UseVisualStyleBackColor = false;
             this.btn_generate.Click += new System.EventHandler(this.button1_Click);
             // 
-            // OpenAI
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.Location = new System.Drawing.Point(126, 261);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.Location = new System.Drawing.Point(126, 290);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox3.Location = new System.Drawing.Point(126, 319);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox4.Location = new System.Drawing.Point(126, 348);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(10, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "max_tokens:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(7, 293);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "temperature:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(7, 322);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "presencePenalty:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(10, 351);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "frequencyPenalty:";
+            // 
+            // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::OAI_APP.Properties.Resources.unnamed;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1502, 599);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btn_generate);
             this.Controls.Add(this.rtb_output);
             this.KeyPreview = true;
-            this.Name = "OpenAI";
+            this.Name = "Form1";
             this.Text = "OpenAI App";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -77,5 +169,13 @@
 
         private RichTextBox rtb_output;
         private Button btn_generate;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
--- OAI-BOT/Form1.cs
+++ OAI-BOT/Form1.cs
@@ -8,25 +8,32 @@ using System.Text;
 using System.Windows.Forms;
 using System.IO;
 using System.Media;
+using System.Text.RegularExpressions;
 using OpenAI_API;
+
 
 namespace OAI_APP
  
 {
     using OAI_APP;
     public partial class Form1 : Form
-    {
- 
+    {   
+
         OpenAIAPI api = new OpenAI_API.OpenAIAPI(APIAuthentication.LoadFromPath());
 
         public bool mute = true;
 
+        public int max_tokens = 100;
+        public double temperature = 0.65;
+        public double presencePenalty = 0.2;
+        public double frequencyPenalty = 0.4;
 
 
         public Form1()
         {
             InitializeComponent();
-
+            System.Drawing.Icon icon = new System.Drawing.Icon("../../../Resources/icon.ico");
+            this.Icon = icon;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -42,7 +49,7 @@ namespace OAI_APP
             }
 
              var result = api.Completions.StreamCompletionAsync(
-                  new CompletionRequest(rtb_output.Text, max_tokens: 100, temperature: 0.65, presencePenalty: 0.3, frequencyPenalty: 0.3),
+                  new CompletionRequest(rtb_output.Text, max_tokens: 100, temperature: 0.65, presencePenalty: 0.2, frequencyPenalty: 0.4),
                   res => rtb_output.Text += res.ToString());
             
           /*  var result = api.Completions.CreateCompletionsAsync(
@@ -109,6 +116,39 @@ namespace OAI_APP
             {
                 mute = !mute;
             }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(textBox1.Text, @"^[0-9]+$") && (textBox1.Text != null) && (textBox1.Text != "") && textBox1.Text[0] != '.')
+            {
+                max_tokens = int.Parse(textBox1.Text);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(textBox2.Text, @"^[0-9.]+$") && (textBox2.Text != null) && (textBox2.Text != "") && textBox2.Text[0] != '.')
+            {
+                temperature = double.Parse(textBox2.Text);
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(textBox3.Text, @"^[0-9.]+$") && (textBox3.Text != null) && (textBox3.Text != "") && textBox3.Text[0] != '.')
+            {
+                presencePenalty = double.Parse(textBox3.Text);
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(textBox4.Text, @"^[0-9.]+$") && (textBox4.Text != null) && (textBox4.Text != "") && textBox4.Text[0] != '.')
+            {
+                frequencyPenalty = double.Parse(textBox4.Text);
+            }
+
         }
     }
 
